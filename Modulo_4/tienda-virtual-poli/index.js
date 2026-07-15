376d2f4a8e0544ceff3189c9e65072643c452d79
Javier <javieer55ph@gmail.com>
2026-07-16 00:01:28 +0100

    categorias

--- Modulo_4/tienda-virtual-poli/index.js
+++ Modulo_4/tienda-virtual-poli/index.js
@@ -8,10 +8,38 @@ const producto = {
 	precio: 90000,
 };
 
+let categorias = [
+	"Arena",
+	"Accesorios",
+	"Servicios",
+]
+
 productos.push(producto);
 
 const buscador                    = document.querySelector('#buscador');
 const listaProductos              = document.querySelector('#lista-productos tbody');
 const formularioRegistrarProducto = document.querySelector('#formulario-registrar-producto');
-const modalRegistrarProducto      = document.querySelector('modal-registrar-producto');
+const modalRegistrarProducto      = document.querySelector('#modal-registrar-producto');
+const selectCategoria		  = document.querySelector('#categoria');
+
+//Evento de carga
+
+document.addEventListener('DOMContentLoaded', () => {
+	listarCategorias();	
+});
+
+//funcion registrar producto
+
+//Renderizar categorias
+
+const listarCategorias = () => {
+	categorias.forEach(categoria => {
+		const option = document.createElement('option');
+		option.value = categoria;
+		option.innerText = categoria;
+
+		selectCategoria.appendChild(option);
+	});
+
+};
 
